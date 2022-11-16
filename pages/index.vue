87bd9181bfbaf6a41528f76159b4cb72af1dc744
phucbm <hiphucbui@gmail.com>
2022-11-16 13:45:34 +0700

    fetch posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,14 @@
 <template>
   <section>
     <h1>Welcome to the homepage</h1>
-    <ol>
-      <PostItem v-for="post in posts" :post="post"/>
-    </ol>
+    <a href="/posts">Posts</a>
   </section>
 </template>
 
 
 <script>
-import PostItem from "../components/PostItem";
-import axios from "axios";
-
 export default {
   name: "index",
-  components: {PostItem},
-  data(){
-    return {
-      posts: []
-    }
-  },
-  mounted(){
-    axios.get(
-        `http://localhost/vuefront/wp-json/wp/v2/posts`
-    ).then(response => {
-      this.posts = response.data
-      console.log(this.posts)
-    });
-  }
 }
 </script>
 
---
+++ pages/posts.vue
@@ -0,0 +1,30 @@
+<template>
+  <ol>
+    <PostItem v-for="post in posts" :post="post"/>
+  </ol>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "post",
+  data(){
+    return {
+      posts: []
+    }
+  },
+  mounted(){
+    axios.get(
+        `https://dev.mochisandbox.com/wordpress-vue/wp-json/wp/v2/posts`
+    ).then(response => {
+      this.posts = response.data
+      console.table(this.posts)
+    });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
