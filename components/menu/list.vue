7a8d9e88001e01c59bc977ee3775fe715276e9a6
phucbm <hiphucbui@gmail.com>
2022-11-22 14:56:55 +0700

    menu: rename

--- components/menu/list.vue
+++ components/menu/list.vue
@@ -1,11 +1,12 @@
-<script setup>/**
+<script setup>
+/**
  * Component <MenuList :location="'main-menu'" :menu="menu" :parentId="id"/>
  */
 const props = defineProps(['location', 'menu', 'parentId']);
 
-const {data: menuFetched} = await useFetch(`/api/menu?location=${props.location}`, {key: props.location});
+const {data: menuFetched} = props.location ? await useFetch(`/api/menu?location=${props.location}`, {key: props.location}) : {};
 
-const menu_final = computed(() => {
+const the_menu = computed(() => {
   let result = props.location ? menuFetched.value : props.menu;
 
   if(props.parentId){
@@ -21,14 +22,14 @@ const menu_final = computed(() => {
 
 <template>
 
-  <div v-if="!menu_final.ok"><strong>{{ menu_final.message }}</strong></div>
+  <div v-if="!the_menu.ok"><strong>{{ the_menu.message }}</strong></div>
 
-  <ul v-if="menu_final.ok && !parentId" class="d-flex fw-wrap" style="gap:40px; list-style:none;">
-    <MenuItem v-for="item in menu_final.items" :item="item" :menu="menu_final"/>
+  <ul v-if="the_menu.ok && !parentId" class="d-flex fw-wrap" style="gap:40px; list-style:none;">
+    <MenuItem v-for="item in the_menu.items" :item="item" :menu="the_menu"/>
   </ul>
 
-  <ul v-if="menu_final.ok && parentId" class="submenu">
-    <MenuItem v-for="item in menu_final.filtered" :item="item" :menu="menu_final" :parentId="parentId"/>
+  <ul v-if="the_menu.ok && parentId" class="submenu">
+    <MenuItem v-for="item in the_menu.filtered" :item="item" :menu="the_menu" :parentId="parentId"/>
   </ul>
 
 
