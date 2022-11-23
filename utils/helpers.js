45b523bafb8e43c7a325eb91263dd18a3f2f5c77
phucbm <hiphucbui@gmail.com>
2022-11-23 16:11:22 +0700

    pages router > detect post types, taxes (clean up)

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,32 +1,34 @@
-import {useRuntimeConfig} from "nuxt/app";
-
-export function getURL(url, prefix = window.location.origin){
-    const runtimeConfig = useRuntimeConfig();
-    return url.replace(runtimeConfig.public.apiBase, prefix)
-}
-
 export function strippedHtml(string){
     let regex = /(<([^>]+)>)/ig;
 
     return string.replace(regex, "");
 }
 
+
+/**
+ * Get post type from slug array
+ * @param slugArray
+ * @returns {string|*}
+ */
 export function getPostTypeFromSlugArray(slugArray){
-    const type = 'page';
     const siteInfo = useState("siteInfo");
     const postTypes = siteInfo.value['post_types']['types'];
     const taxonomies = siteInfo.value['post_types']['taxes'];
     const terms = siteInfo.value['post_types']['terms'];
 
-    let isPostType = postTypes.includes(slugArray[0]);
-    let isTax = taxonomies.includes(slugArray[0]);
-    let isTerm = terms.includes(slugArray[0]);
-    let isPost = slugArray[0] === siteInfo.value.posts_page_slug;
-
-
-    if(isPost) return siteInfo.value.posts_page_slug;
+    // CPT page
+    const isPostType = postTypes.includes(slugArray[0]);
     if(isPostType) return slugArray[0];
+
+    // posts page
+    const isPost = slugArray[0] === siteInfo.value.posts_page_slug;
+    if(isPost) return siteInfo.value.posts_page_slug;
+
+    // taxonomy page
+    const isTax = taxonomies.includes(slugArray[0]);
     if(isTax) return slugArray[0];
+
+    //let isTerm = terms.includes(slugArray[0]);
 
     //console.log(slugArray.length, isPostType, isTax)
 
